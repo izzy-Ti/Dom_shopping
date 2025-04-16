a9e922fa5cf8e8342227f2dec57c78bc8067feb9
israel ashenafi <israelashenafi@gmail.com>
2025-04-16 12:00:10 -0700

    Dom_shopping

---
+++ store/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def store(request):
+    return render(request, 'store/store.html')
+def cart(request):
+    return render(request, 'store/store.html')
+def checkout(request):
+    return render(request, 'store/checkout.html')
